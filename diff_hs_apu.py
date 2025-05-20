905e23567d5a2af0f4f9a43354c406d20a9b13d9
Taylor Coakley <coakley.64@osu.edu>
2025-05-20 14:13:14 -0400

    adding func for the differences

---
+++ diff_hs_apu.py
@@ -0,0 +1,189 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd 
+import seaborn as sns; sns.set()
+import time
+from matplotlib.colors import LinearSegmentedColormap
+import numpy as np
+
+# _____________________________________________________________________________________ # 
+# 
+#                                 USER INTERFACE
+# Change the CSV file destinations to where you need them to be
+# _____________________________________________________________________________________ #
+
+
+hswapsurf = 'bemco_hs.csv' # hotswap
+surfdata = 'bemco_surfs.csv' # surfs
+turfiodata = 'turf_bemco.csv' # turfios/turf
+
+window_size = 100  # Adjust do what you want 
+
+# Colors for the  graph
+colors = [ "#C76D6D", "#9C4A6F", "#A067A5", "#8357A4", "#6D5792", "#46688D", "#5F9EA0", 
+          "#4E7E7B", "#688E4E", "#3C665A", "#7F885C", "#9C8566", "#5C6A82", "#318C8C" ]
+
+start = pd.to_datetime('1900-01-01 15:20:03')
+end = pd.to_datetime('1900-01-01 18:39:46')
+
+# _____________________________________________________________________________________ # 
+# 
+#                                 DAQ ORDER
+# This shows the different orders of the SURFs/TURFIOs in the crates
+# _____________________________________________________________________________________ #
+
+# Hotswap order
+hs_HDAQ_order = [ '7HS_3', '6HS_3', '5HS_3', '4HS_3', '3HS_3', '2HS_3', '1HS_3', '1HS_2', '2HS_2', '3HS_2', '4HS_2', '5HS_2', '6HS_2', '7HS_2' ]
+hs_VDAQ_order = [ '6HS_1', '5HS_1', '4HS_1', '3HS_1', '2HS_1', '1HS_1', '1HS_0', '2HS_0', '3HS_0', '4HS_0', '5HS_0', '6HS_0']
+
+# All the SURFs in the DAQ
+HDAQ_order = [ 10, 20, 29, 13, 25, 32, 23, 12, 21, 31, 26, 7, 5, 28 ]
+VDAQ_order = [ 15, 16, 14, 22, 27, 19, 9, 8, 30, 11, 33, 24 ]
+
+
+# Save the strings as written in CSV (I didn't set this)
+for iter in range(len(hs_HDAQ_order)): 
+    hs_HDAQ_order[iter] = 'T_SURF' + str(hs_HDAQ_order[iter])
+for iter in range(len(hs_VDAQ_order)): 
+    hs_VDAQ_order[iter] = 'T_SURF' + str(hs_VDAQ_order[iter])
+
+
+apu_HDAQ_order, apu_VDAQ_order = [], []
+for iter in range(len(HDAQ_order)): 
+    apu_HDAQ_order.append('T_APU_SURF_' + str(HDAQ_order[iter]))
+for iter in range(len(VDAQ_order)): 
+    apu_VDAQ_order.append('T_APU_SURF_' + str(VDAQ_order[iter]))
+
+
+# _____________________________________________________________________________________ # 
+# 
+#                             VARIABLES
+# This takes all the data points and saves them under a dictionary with each SURF being
+# a label
+# 
+# _____________________________________________________________________________________ #
+
+# Dictionaries 
+hotswapsurf = {}
+apusurf = {}
+turfios = {}
+
+
+#_______________________________________________________________________________________# 
+#
+#                             FUNCTIONS 
+#
+#_______________________________________________________________________________________#
+
+# Sort by times
+def sortvals(df): 
+    df['time'] = pd.to_datetime(df['time'], format = '%I:%M:%S %p')
+    return df.sort_values(by='time')  
+
+# Creates nested dictionaries
+def grouping(df, dictval): 
+    for sensor, group in df.groupby('sensor'):
+        dictval[sensor] = dict(zip(group['time'], group['temperature'])) 
+    return dictval
+
+# RMS Smoothing the data
+def smoothing(order, dictval): 
+    crate = {}
+    error = []
+    filtered_times = []
+    for key in order:
+        if key in dictval:  
+            sensor_data = dictval[key]  
+            df = pd.DataFrame(sensor_data.items(), columns=['time', 'temperature'])
+            df['time'] = pd.to_datetime(df['time'], format = '%I:%M:%S %p')
+            df_filter = df[(df['time'] >= start) & (df['time'] <= end)]
+            df['rms_smooth'] = df['temperature'].rolling(window=window_size).apply(lambda x: np.sqrt(np.mean(x**2)), raw=True)
+            error.append(df_filter['temperature'].std())
+            filtered_times.append(df_filter['time'])
+            crate[key] = dict(zip(df['time'], df['rms_smooth']))
+    return crate, error, filtered_times
+
+
+#_______________________________________________________________________________________# 
+#
+#                             EXECUTABLE  
+#
+#_______________________________________________________________________________________#
+
+
+# Read in all the CSV files 
+dfhssrf = pd.read_csv(hswapsurf) # hotswap
+dftfio = pd.read_csv(turfiodata) # turfio/turf
+dfsrf = pd.read_csv(surfdata)    # surf 
+
+# Iterate over the different boards to sort over time
+board = [ dfhssrf, dfsrf, dftfio]
+
+for iter in range(len(board)): 
+    board[iter] = sortvals(board[iter])
+
+# Groups HDAQ and VDAQ 
+hotswapsurf = grouping(dfhssrf, hotswapsurf)
+turfios = grouping(dftfio, turfios)
+apusurf = grouping(dfsrf, apusurf)
+
+# Applies RMS smoothing because that data sure is noisy :) 
+horzhotswapsurfs, horzhotswaperr, horzhotswaperrtime = smoothing(hs_HDAQ_order, hotswapsurf)
+verthotswapsurfs, verthotswaperr, verthotswaperrtime = smoothing(hs_VDAQ_order, hotswapsurf)
+horzapusurfs, horzapuerr, horzapuerrtime = smoothing(apu_HDAQ_order, apusurf)
+vertapusurfs, vertapuerr, vertapuerrtime = smoothing(apu_VDAQ_order, apusurf)
+
+
+
+#_______________________________________________________________________________________# 
+#
+#                             TIME TO GRAPH IT, YO  
+#
+#_______________________________________________________________________________________#
+
+
+def graphygraphtwo(apudata, hsdata,  error = False, title = False, num = False): 
+
+    _, ax = plt.subplots()
+      
+    i = 0
+
+    
+    apu_vals = np.array(list(apudata.values()))
+    hs_vals = np.array(list(hsdata.values()))
+    
+    # Plot filtered data
+    # ax.plot(list(apudata.keys()), apu_vals, '#9C4A6F', label = 'APU' )
+    #  ax.plot(list(hsdata.keys()), hs_vals, "#688E4E",  label = 'Hot Swap' )
+    ax.plot(list(hsdata.keys())[0:1820], apu_vals - hs_vals[0:1820], 'k', label = 'Difference')
+    ax.plot()
+
+    ax.xaxis.set_major_locator(matplotlib.dates.AutoDateLocator())  # Auto-adjust tick frequency
+    ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%I:%M:%S %p'))  # 12-hour format
+    ax.xaxis.set_label_text('Time')
+    ax.yaxis.set_label_text('Temperature (C)')
+
+    if title != False: 
+        plt.title(title)
+
+    plt.legend( ncol = 3, loc = 'upper right', fontsize = 10, frameon = True, edgecolor = 'black', 
+               facecolor = 'lightgray', columnspacing = 1.5 )
+
+    plt.show()
+
+"""for key in horzapusurfs: 
+   for iter in range(len(HDAQ_order)):
+        if str(HDAQ_order[iter]) in key and str(HDAQ_order): 
+            """
+new_dict = {}
+
+for old_key, new_key in zip(horzhotswapsurfs.keys(), HDAQ_order):
+    new_dict[str(new_key)] = horzhotswapsurfs[old_key]  # Map old values to new keys
+
+# Replace original dictionary
+horzhotswapsurfs = new_dict
+for key1 in horzhotswapsurfs:
+    for key2 in horzapusurfs:
+        for iter in range(len(HDAQ_order)): 
+            if str(HDAQ_order[iter]) in key1 and str(HDAQ_order[iter]) in key2: 
+                graphygraphtwo(horzapusurfs[key2], horzhotswapsurfs[key1])
